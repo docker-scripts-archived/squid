c4e4d5383508c87be3394af6183431c0921d1b8f
dashohoxha <dashohoxha@gmail.com>
2018-05-13 15:13:40 +0200

    Fix comments for gmail account

--- settings.sh
+++ settings.sh
@@ -2,8 +2,8 @@ APP=squid
 IMAGE=squid
 CONTAINER=squid
 
-### Gmail account for notifications.
-### Make sure to enable less-secure-apps:
-### https://support.google.com/accounts/answer/6010255?hl=en
+### Gmail account for notifications. This will be used by ssmtp.
+### You need to create an application specific password for your account:
+### https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882
 GMAIL_ADDRESS=
 GMAIL_PASSWD=
